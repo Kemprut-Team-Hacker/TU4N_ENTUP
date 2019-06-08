6994ca5eb0d13e717889f6a6f29696b71f5a4543
Kemprut-Team-Hacker <51508390+Kemprut-Team-Hacker@users.noreply.github.com>
2019-06-09 01:11:13 +0700

    Tools serbaguna

---
+++ Serbaguna.sh
@@ -0,0 +1,139 @@
+clear
+figlet TU4N-ENTUP | lolcat
+echo "===============================================
+
+•Nama    : TU4N_ENTUP
+•Team    : Kemprut-Team-Hacker
+•Github  : https://github.com/Kemprut-Team-Hacker
+
+================================================" | lolcat
+
+sleep 2
+
+echo "(0) FOLLOWERS IG AKTIF
+(1)•HACK FB TARGET
+(2)•SADAP WA
+(3)•LACAK LOKASI
+(4)•HACK WIFI(root)
+(5)•DIAMOND ILEGAL FREE FIRE
+(6)•AUTO SUBSCRIBE YOUTUBE
+(7)•PHISING ALL MEDSOS
+(8)•PULSA GRATIS
+(9)•EXIT" | lolcat
+echo "•PILIH NOMOR DULU SAYANG : " | lolcat
+read nomor
+
+if [ $nomor = 0 ] || [ $nomor = 00 ]
+then
+clear
+figlet INSTAGRAM | lolcat
+cowsay TOOLS BELUM SELESAI TUNGGU 1 MINGGU LAGI 
+pkg install git
+pkg install python
+pkg install cowsay
+pkg install toilet
+pip2 install mechanize
+fi
+
+if [ $nomor = 1 ] || [ $nomor = 01 ]
+then
+clear
+figlet FACEBOOK | lolcat
+cowsay TOOLS BELUM SELESAI TUNGGU 1 MINGGU LAGI
+pkg install git
+pkg install python
+pkg install cowsay
+pkg install toilet
+pip2 install mechanize
+fi
+
+if [ $nomor = 2 ] || [ $nomor = 02 ]
+then
+clear
+figlet WA SAKTI | lolcat
+cowsay TOOLS BELUM SELESAI TUNGGU 1 MINGGU LAGI
+pkg install git
+pkg install python
+pkg install cowsay
+pkg install toilet
+pip2 install mechanize
+fi
+
+if [ $nomor = 3 ] || [ $nomor = 03 ]
+then
+clear
+figlet LACAK LOKASI | lolcat
+cowsay TOOLA BELUM SELESAI TUNGGU 1 MINGGU LAGI
+pkg install git
+pkg install python
+pkg install cowsay
+pkg install toilet
+pip2 install mechanize
+fi
+
+if [ $nomor = 4 ] || [ $nomor = 04 ]
+then
+clear
+figlet WIFI | lolcat
+cowsay TOOLS BELUM SELESAI TUNGGU 1 MINGGU LAGI
+pkg install git
+pkg install python
+pkg install cowsay
+pkg install toilet
+pip2 install mechanize
+fi
+
+if [ $nomor = 5 ] || [ $nomor = 05 ]
+then
+clear
+figlet FREE FIRE | lolcat
+cowsay TOOLS BELUM SELESAI TUNGGU 1 MINGGU LAGI
+pkg install git
+pkg install python
+pkg install cowsay
+pkg install toilet
+pip2 install mechanize
+fi
+
+if [ $nomor = 6 ] || [ $nomor = 06 ]
+then
+clear
+figlet YOUTUBE | lolcat
+cowsay TOOLS BELUM SELESAI TUNGGU 1 MINGGU LAGI
+pkg install git
+pkg install python
+pkg install cowsay
+pkg install toilet
+pip2 install mechanize
+fi
+
+if [ $nomor = 7 ] || [ $nomor = 07 ]
+then
+clear
+figlet PHISING | lolcat
+cowsay TOOLS BELUM SELESAI TUNGGU 1 MINGGU LAGI
+pkg install git
+pkg install python
+pkg install cowsay
+pkg install toilet
+pip2 install mechanize
+fi
+
+if [ $nomor = 8 ] || [ $nomor = 08 ]
+then
+clear
+figlet PULSA | lolcat
+cowsay TOOLS BELUM SELESAI TUNGGU 1 MINGGU LAGI
+pkg install git
+pkg install python
+pkg install cowsay
+pkg install toilet
+pip2 install mechanize
+fi
+
+if [ $nomor = 9 ] || [$nomor = 09 ]
+then
+clear
+figlet EXIT
+cowsay SAMPAI JUMPA | lolcat
+fi
