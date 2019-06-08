ff7a8fe4ef04fd8509b7592a7f8cbd0ee22c15de
Kemprut-Team-Hacker <51508390+Kemprut-Team-Hacker@users.noreply.github.com>
2019-06-08 20:15:43 +0700

    New TOOLS
    Gunakan dengan bijak

---
+++ TU4N_ENTUP.sh
@@ -0,0 +1,73 @@
+clear
+figlet TU4N-ENTUP | lolcat
+echo "===============================================
+
+•Nama : TU4N_ENTUP
+•Team : Kemprut-Hacker-Team
+
+================================================"  | lolcat
+
+sleep 2
+
+echo "(1) FOLLOWERS IG AKTIF
+(2)•HACK FB TARGET
+(3)•SADAP WA
+(4)•LACAK LOKASI
+(5)•HACK WIFI(root)
+(6)•DIAMOND ILEGAL FREE FIRE
+(7)•AUTO SUBSCRIBE YOUTUBE
+(8)•PHISING ALL MEDSOS
+(9)•EXIT" | lolcat
+echo "•PILIH NOMOR DULU SAYANG : " | lolcat
+read nomor
+
+if [ $nomor = 1 ] || [ $nomor = 01 ]
+then
+clear
+figlet INSTAGRAM | lolcat
+echo "Masukan nomor target :" | lolcat
+pkg install git
+git clone https://github.com/Kemprut-Team-Hacker/TU4N_ENTUP
+fi
+
+if [ $nomor = 2 ] || [ $nomor = 02 ]
+then
+clear
+figlet FB TARGET  | lolcat
+pkg install curl
+fi
+
+if [ $nomor = 3 ] || [ $nomor = 03 ]
+then
+clear
+figlet SADAP WA | lolcat
+pkg install figlet
+fi
+
+if [ $nomor = 4 ] || [ $nomor = 04 ]
+then
+clear
+figlet LACAK LOKASI | lolcat
+pkg install python2
+fi
+
+if [ $nomor = 5 ] || [ $nomor = 05 ]
+then
+clear
+figlet WIFI | lolcat
+pkg install nano
+fi
+
+if [ $nomor = 6 ] || [ $nomor = 06 ]
+then
+clear
+figlet FREE FIRE | lolcat
+pkg install php
+fi
+
+if [ $nomor = 7 ] || [ $nomor = 07 ]
+then
+clear
+figlet YOUTUBE | lolcat
+pkg install bs4
+fi
